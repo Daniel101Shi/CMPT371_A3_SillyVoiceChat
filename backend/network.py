8cb8218f28398e39ddfb0b702892eaea818988fc
Daniel <daniel101shi@gmail.com>
2026-04-06 19:39:49 -0700

    added data recieving logic to network.py

--- backend/network.py
+++ backend/network.py
@@ -1,3 +1,7 @@
+
+# test: run python3 network.py in one terminal
+# run: echo "Hello from terminal 2" | nc -u 127.0.0.1 5000 ,in another terminal
+
 import socket
 # set address and port
 LOCAL_IP = "127.0.0.1"
@@ -11,3 +15,14 @@ sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
 sock.bind((LOCAL_IP, LOCAL_PORT))
 
+# core logic
+while True:
+
+    # recieve data
+    data, address = sock.recvfrom(4096)
+
+    # decode data from bytes to string
+    msg = data.decode("utf-8")
+    
+    # print recieved msg
+    print(f"Recieved from {address}: {msg}")
