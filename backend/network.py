190d42a818fc8de34c5ebba6ba3328bd03618e55
Daniel <daniel101shi@gmail.com>
2026-04-06 20:23:43 -0700

    impleted a jitter buffer in network.py

--- backend/network.py
+++ backend/network.py
@@ -15,6 +15,13 @@ sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
 sock.bind((LOCAL_IP, LOCAL_PORT))
 
+# waiting list for incoming data
+jitter_buffer = []
+# how many packets we wait for before starting to play audio 
+JITTER_BUFFER_SIZE = 3
+#the last ID we played
+last_played_seq = -1
+
 # core logic
 while True:
 
@@ -24,12 +31,25 @@ while True:
     # unpack the first 4 bytes as sequence number
     header = data[:4]
     seq_num = struct.unpack("!I", header)[0]
-
-
     # get the actual message body
     msg_body = data[4:].decode("utf-8")
-    
     # print recieved msg
     print(f"Recieved from {address}: {seq_num} - {msg_body}")
+
+    # check if the packet shoudl be skipped
+    if(seq_num <= last_played_seq):
+        continue
+    else:
+        jitter_buffer.append((seq_num, msg_body))
+        jitter_buffer.sort(key=lambda x: x[0])
+
+    # play audio if buffer is full
+    if(len(jitter_buffer) >= JITTER_BUFFER_SIZE):
+        popped_seq, popped_msg = jitter_buffer.pop(0)
+        last_played_seq = popped_seq
+        print(f"Playing packet #{popped_seq}: {popped_msg}")
+    
+
+    
 
 
